cbb6c744084bab015bdce398f7a94d7f8916ec7e
PortalCube <35104213+PortalCube@users.noreply.github.com>
2024-03-17 21:40:43 +0900

    feat: 모바일 웹페이지 Wrapper 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { BrowserRouter as Router, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import styled from 'styled-components';
 import routes from './routes.jsx';
 import store from './redux/store.js';
+import MobileWrapper from './MobileWrapper.jsx';
 
 const Container = styled.div`
     height: 100%;
@@ -12,9 +12,7 @@ function App() {
     return (
         <Provider store={store}>
             <Container>
-                <Router>
-                    <Routes>{routes}</Routes>
-                </Router>
+                <MobileWrapper />
             </Container>
         </Provider>
     );
