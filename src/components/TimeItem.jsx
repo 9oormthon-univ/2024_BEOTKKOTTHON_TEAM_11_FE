354ca5c777bde6218503d9412b559f3f5261d3c9
PortalCube <35104213+PortalCube@users.noreply.github.com>
2024-03-18 20:06:31 +0900

    feat: 타임 테이블 컴포넌트 UI 인터렉션 개선

---
+++ src/components/TimeItem.jsx
@@ -0,0 +1,62 @@
+import classNames from 'classnames';
+import styled from 'styled-components';
+
+const Item = styled.div`
+    height: 18px;
+    border: 1px solid #0000001f;
+    display: flex;
+    background-color: #ffffff;
+    user-select: none;
+
+    border-top: 1px dashed #0000001f;
+    border-bottom: 1px dashed #0000001f;
+
+    transition: background-color 0.1s;
+
+    font-size: 12px;
+    align-items: center;
+    justify-content: center;
+    color: #0000006f;
+
+    cursor: default;
+
+    &.active {
+        background-color: #0fa866;
+    }
+
+    &:nth-child(2n) {
+        border-top: 1px solid #0000006f;
+    }
+`;
+
+const TimeItem = ({
+    active,
+    text,
+    onTouchStart,
+    onTouchEnd,
+    onTouchMove,
+    onMouseMove,
+    onMouseDown,
+    onMouseUp,
+    style,
+}) => {
+    return (
+        <Item
+            style={style}
+            className={classNames({ active })}
+            {...{
+                onTouchStart,
+                onTouchEnd,
+                onTouchMove,
+                onMouseMove,
+                onMouseDown,
+                onMouseUp,
+            }}
+            draggable={false}
+        >
+            {text}
+        </Item>
+    );
+};
+
+export default TimeItem;
