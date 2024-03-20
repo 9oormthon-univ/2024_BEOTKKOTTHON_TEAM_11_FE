b5563b14aa011351f3869d380a09bf4f4c4ea905
PortalCube <35104213+PortalCube@users.noreply.github.com>
2024-03-20 11:17:42 +0900

    chore: dayjs 언어를 한국어로 설정

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import dayjs from 'dayjs';
+
+import 'dayjs/locale/ko';
+dayjs.locale('ko'); // use locale globally
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
