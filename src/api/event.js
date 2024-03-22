203308006056687e730e5f7bd42dc4995f482991
PortalCube <35104213+PortalCube@users.noreply.github.com>
2024-03-22 16:55:13 +0900

    refactor: createParty 함수를 createEvent로 rename

--- src/api/event.js
+++ src/api/event.js
@@ -1,4 +1,24 @@
-export async function createParty(request) {
+export async function createEvent(request) {
+    // const axios = getAxiosInstance();
+
+    // const bodyData = {
+    //     ownerId: request.userId,
+    //     title: request.name,
+    //     startDate: request.startDate,
+    //     locName: request.location,
+    //     locAddress: request.address,
+    //     memo: request.memo,
+    // };
+
+    // const response = await axios.post('/api/v1/promises', bodyData);
+    // return response.data;
+
+    return {
+        url: 'https://example.com',
+    };
+}
+
+export async function getParty(request) {
     // const axios = getAxiosInstance();
 
     // const bodyData = {
