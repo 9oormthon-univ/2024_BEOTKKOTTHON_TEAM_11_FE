28c87d02b1bba5acc9834ae74860aa434bb569bf
Hyunah Kim <hyunah2765@gmail.com>
2024-03-22 17:28:54 +0900

    feat: 확정된 밥약 컴포넌트 호버시 스타일 변화 적용

--- src/components/ConfirmBox.jsx
+++ src/components/ConfirmBox.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import dummy from '../db/confirmdata.json'
 import { IoIosCalendar, IoMdPin, IoMdPeople, IoIosArrowForward } from "react-icons/io";
 import { BsFillBookmarkStarFill, BsFillBookmarkPlusFill  } from "react-icons/bs";
 
@@ -8,122 +7,120 @@ const BabDiv = styled.div`
     width: 345px;
     height: 180px;
     border-radius: 8px;
-    background-color: #FFF6F6;
+    background-color: ${props => props.hover ? '#FE5858' : '#FFF6F6'};
+    color: ${props => props.hover ? '#FFF6F6' : '#FE5858'};
     margin: 34px auto;
     padding: 12px 19px 0 19px;
     box-shadow: 0px 4px 4px rgba(214, 73, 73, 0.15);
+    transition: background-color 0.3s, color 0.3s;
 `;
-//디데이+이름, 마크
+
 const DNMContainer = styled.div`
-    display: flex; //자식 요소들을 한 줄에 나열합니다.
+    display: flex;
     align-items: center;
     justify-content: space-between;
 `;
-//디데이+이름
+
 const DdayNameContainer = styled.div`
-    display: flex; //자식 요소들을 한 줄에 나열합니다.
+    display: flex;
     align-items: center;
 `;
-//디데이
+
 const DDday = styled.p`
-    color: #FFF6F6;
-    background-color: #FE5858;
+    background-color: ${props => props.hover ? '#FFF6F6' : '#FE5858'};
+    color: ${props => props.hover ? '#FE5858' : '#FFF6F6'};
     width: 62px;
     height: 24px;
     border-radius: 8px;
-    display: flex;            // flex 컨테이너 설정
-    justify-content: center;  // 수평 중앙 정렬
-    align-items: center;      // 수직 중앙 정렬
+    display: flex;
+    justify-content: center;
+    align-items: center;
     font-weight: 600;
     font-size: 16px;
     margin-right: 10px;
 `;
 
-//파티장인지 파티원인지 구분마크, 밥약 이름
 const MarkContainer = styled.div`
-    color: #FE5858;
+    color: ${props => props.hover ? '#FFF6F6' : '#FE5858'};
+    
     width: 24px;
     height: 24px;
 `;
 const BabName = styled.p`
     font-weight: 600;
     font-size: 24px;
-    color: #FE5858;
-    margin-bottom: 10px; // 아래쪽 여백 추가
+    color: ${props => props.hover ? '#FFF6F6' : '#FE5858'};
+    margin-bottom: 10px;
     padding-top: 4px;
 `;
 
-//날짜
+const iconStyle = hover => `
+    color: ${hover ? '#FFF6F6' : '#FE5858'};
+    width: 24px;
+    height: 24px;
+    margin-right: 8px;
+`;
+
 const DateContainer = styled.div`
     display: flex;
-    align-items: center; // Flex 아이템들을 수직 중앙 정렬
-    margin-left: 8px; // 아이콘과 날짜의 좌측 여백
+    align-items: center;
+    margin-left: 8px;
     margin-top: 2px;
 `;
 const StyledCalendarIcon = styled(IoIosCalendar)`
-    color: #FE5858;
-    width: 24px;
-    height: 24px;
-    margin-right: 8px; // 아이콘과 날짜 사이의 간격 조정
+${({ hover }) => iconStyle(hover)}
 `;
 const ConfirmDate = styled.p`
     font-weight: 600;
     font-size: 16px;
-    color: #FE5858;
+    color: ${props => props.hover ? '#FFF6F6' : '#FE5858'};
 `;
 
-//장소
 const PlaceContainer = styled.div`
     display: flex;
-    align-items: center; // Flex 아이템들을 수직 중앙 정렬
-    margin-left: 8px; // 아이콘과 날짜의 좌측 여백
+    align-items: center;
+    margin-left: 8px;
     margin-top: 12px;
 `;
 const StyledPinIcon = styled(IoMdPin)`
-    color: #FE5858;
-    width: 24px;
-    height: 24px;
-    margin-right: 7px; // 아이콘과 장소 사이의 간격 조정
+${({ hover }) => iconStyle(hover)}
+    margin-right: 7px;
 `;
 const ConfirmPlace = styled.p`
     font-weight: 600;
     font-size: 16px;
-    color: #FE5858;
+    color: ${props => props.hover ? '#FFF6F6' : '#FE5858'};
 `;
-//사람
+
 const PeopleContainer = styled.div`
     display: flex;
-    align-items: center; // Flex 아이템들을 수직 중앙 정렬
-    margin-left: 8px; // 아이콘과 날짜의 좌측 여백
-    
+    align-items: center;
+    margin-left: 8px;
 `;
 const StyledPeopleIcon = styled(IoMdPeople)`
-    color: #FE5858;
-    width: 24px;
-    height: 24px;
-    margin-right: 8px; // 아이콘과 장소 사이의 간격 조정
+${({ hover }) => iconStyle(hover)}
+    margin-right: 8px;
 `;
-//파티장 이름
+
 const ConfirmPerson = styled.p`
     font-weight: 600;
     font-size: 16px;
-    color: #FE5858;
+    color: ${props => props.hover ? '#FFF6F6' : '#FE5858'};
 `;
-//파티원들 이름
+
 const ConfirmPeople = styled.p`
     font-weight: 600;
     font-size: 10px;
-    color: #FE5858;
+    color: ${props => props.hover ? '#FFF6F6' : '#FE5858'};
     margin-left: 6px;
 `;
 
-
-// arrowm button
 const Button = styled.button`
   padding: 4px;
   background: none;
   border: none;
   border-radius: 50%;
+
   display: flex;
   align-items: center;
   justify-content: center;
@@ -137,7 +134,7 @@ const Button = styled.button`
   & > svg {
       width: 24px;
       height: 24px;
-      color: #FE5858;
+      color: ${props => props.hover ? '#FFF6F6' : '#FE5858'};
   }
 
   &.disabled {
@@ -145,9 +142,9 @@ const Button = styled.button`
       pointer-events: none;
   }
 `;
-//인원+화살표 버튼
+
 const PeopleArrowContainer = styled.div`
-  display: flex; //자식 요소들을 한 줄에 나열합니다.
+  display: flex;
   align-items: center;
   justify-content: space-between;
   margin-top: 8px;
@@ -156,32 +153,31 @@ const PeopleArrowContainer = styled.div`
 
 function ConfirmBox(props) {
     const { eventName, date, time, place, host, participants, userRole } = props.event;
+    const [hover, setHover] = useState(false);
 
-    // D-Day 계산
     const calculateDDay = (eventDate) => {
         const today = new Date();
-        today.setHours(0, 0, 0, 0); // 오늘 날짜를 자정으로 설정
-    
+        today.setHours(0, 0, 0, 0);
+
         const eventDay = new Date(eventDate);
-        eventDay.setHours(0, 0, 0, 0); // 이벤트 날짜를 자정으로 설정
-    
+        eventDay.setHours(0, 0, 0, 0);
+
         const timeDiff = eventDay - today;
         const dayDiff = timeDiff / (1000 * 60 * 60 * 24);
-    
-        return Math.ceil(dayDiff); // 오늘을 포함하려면 올림을 사용
+
+        return Math.ceil(dayDiff);
     };
 
     const dDay = calculateDDay(date);
   return (
-    <BabDiv>
+    <BabDiv onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} hover={hover}>
         <DNMContainer>
             <DdayNameContainer>
-                {/* D-Day 표시 추가 */}
-                <DDday>{dDay === 0 ? 'D-Day' : `D-${dDay}`}</DDday>
-                <BabName>{eventName}</BabName>
+                <DDday hover={hover}>{dDay === 0 ? 'D-Day' : `D-${dDay}`}</DDday>
+                <BabName hover={hover}>{eventName}</BabName>
             </DdayNameContainer>
             
-            <MarkContainer>
+            <MarkContainer hover={hover}>
                 {userRole === 'host' ? (
                     <BsFillBookmarkStarFill size={24}/>
                     ) : (
@@ -192,30 +188,31 @@ function ConfirmBox(props) {
         
        
         <DateContainer>
-            <StyledCalendarIcon />
-            <ConfirmDate>{`${date} ${time}`}</ConfirmDate>
+            <StyledCalendarIcon hover={hover} />
+            <ConfirmDate hover={hover}>{`${date} ${time}`}</ConfirmDate>
         </DateContainer>
 
         <PlaceContainer>
-            <StyledPinIcon /><ConfirmPlace>{place}</ConfirmPlace>
+            <StyledPinIcon hover={hover} /><ConfirmPlace hover={hover}>{place}</ConfirmPlace>
         </PlaceContainer>
 
         <PeopleArrowContainer>
             <PeopleContainer>
-                <StyledPeopleIcon />
-                <ConfirmPerson>{host}</ConfirmPerson>
-                <ConfirmPeople>{participants.join(', ')}</ConfirmPeople>
+                <StyledPeopleIcon hover={hover} />
+                <ConfirmPerson hover={hover}>{host}</ConfirmPerson>
+                <ConfirmPeople hover={hover}>{participants.join(', ')}</ConfirmPeople>
                 
             </PeopleContainer>
-            <Button>
-                <IoIosArrowForward/>
+            <Button hover={hover} className={userRole !== 'host' ? 'disabled' : ''}>
+                <IoIosArrowForward />
             </Button>
         </PeopleArrowContainer>
     </BabDiv>
-  )
+  );
 }
 
 export default ConfirmBox;
+
 
 
 
